24a1355ef6cdcc9a27aa4bb10a9f2746cf47f7a4
Chioma <chiomacodes@gmail.com>
2023-01-05 14:22:14 +0100

    Commit 1: My very first commit

---
+++ JS/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// Open and Close Navbar Menu
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const bgOverlay = document.querySelector(".overlay");
+const homeClick = document.querySelector(".home-click");
+const acctClick = document.querySelector(".acct-click");
+const helpClick = document.querySelector(".help-click");
+const cartClick = document.querySelector(".cart-click");
+const homeDD = document.querySelector(".hm-dd");
+const accountDD = document.querySelector(".ac-dd");
+const helpDD = document.querySelector(".hp-dd");
+const cartDD = document.querySelector(".ct-dd");
+
+if (burgerMenu && bgOverlay) {
+   burgerMenu.addEventListener("click", () => {
+      navbarMenu.classList.add("is-active");
+      bgOverlay.classList.toggle("is-active");
+   });
+
+   bgOverlay.addEventListener("click", () => {
+      navbarMenu.classList.remove("is-active");
+      bgOverlay.classList.toggle("is-active");
+   });
+}
+
+// Close Navbar Menu on Links Click
+document.querySelectorAll(".menu-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      navbarMenu.classList.remove("is-active");
+      bgOverlay.classList.remove("is-active");
+   });
+});
+
+// Open and Close Search Bar Toggle
+const searchBlock = document.querySelector(".search-block");
+const searchToggle = document.querySelector(".search-toggle");
+const searchCancel = document.querySelector(".search-cancel");
+
+if (searchToggle && searchCancel) {
+   searchToggle.addEventListener("click", () => {
+      searchBlock.classList.add("is-active");
+   });
+
+   searchCancel.addEventListener("click", () => {
+      searchBlock.classList.remove("is-active");
+   });
+}
